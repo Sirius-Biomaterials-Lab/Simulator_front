3e84100d48c9660f7d45ab5e765d2a1fd2390d16
Artem <artiebears@mail.ru>
2024-11-27 11:28:26 +0300

    setup gh-pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '' ,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
 })
